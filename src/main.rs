c21db9e4415079ce9dc8e6d53a3e5055c4d3d78b
Ian Fisher <ian@iafisher.com>
2024-07-21 13:50:58 -0400

    rust arg parsing and subprocess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::Read;
+use std::process::{Command, Stdio};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: check if running on Linux
+    let args = Args::parse();
+    // TODO: set up anyhow
+    let cmd: std::process::Child = Command::new("echo")
+        .args(args.args)
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let mut stderr = cmd.stderr.unwrap();
+
+    // TODO: optimal buffer size?
+    let mut buffer = [0; 512];
+    loop {
+        // TODO: error handling
+        let n = stderr.read(&mut buffer).unwrap();
+        if n == 0 {
+            break;
+        }
+        println!("read: {:?}", &buffer[0..n]);
+    }
 }
